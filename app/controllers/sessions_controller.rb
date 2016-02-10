1aae50fcb5f467a35f006bb055d2e8fb10e3e954
Jenna Espezua <jennaespezua@gmail.com>
2016-02-10 14:58:16 -0500

    Complete user registration and login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(username: params[:sessions][:username])
-    if user && user.authenticate(params[:sessions][:password])
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to root_path
     else
